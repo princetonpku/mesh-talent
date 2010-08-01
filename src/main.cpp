509eba7a12491d65b95843117a1d3a95e65423d8
jixuxjixu <jixuxjixu@a82818a5-b46c-644e-64b0-fcf3f0ab0d33>
2010-08-01 01:55:23 +0000

    remove glut because of the "exit redefinition promblem".

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <QApplication>
-#include <GL/glut.h>
+//#include <GL/glut.h>
 
 #include "MainWindow.h"
 
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
-	glutInit(&argc, argv);
+	//glutInit(&argc, argv);
 	MainWindow mainWin;
 	mainWin.resize(640, 480);
 	mainWin.showMaximized();
